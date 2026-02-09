0d448cbcd72817b398ebeea93ba25b043c7bc4ec
Wint3rNight <pratap2003singh@gmail.com>
2026-02-10 02:44:06 +0530

    Fire

---
+++ Assets/Scripts/Core/Player/LaunchProjectile.cs
@@ -0,0 +1,120 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class LaunchProjectile : NetworkBehaviour
+{
+    [SerializeField] private InputReader inputReader;
+    [SerializeField] private Transform projectileSpawnPoint;
+    [SerializeField] private GameObject serverProjectilePrefab;
+    [SerializeField] private GameObject clientProjectilePrefab;
+    [SerializeField] private GameObject flashPrefab;
+    [SerializeField] private Collider2D playerCollider;
+    
+    [SerializeField] private float projectileSpeed;
+    [SerializeField] private float fireRate;
+    [SerializeField] private float flashDuration;
+    
+    private bool _shouldFire;
+    private float _prevFireTime;
+    private float _flashTimer;
+    
+    
+    public override void OnNetworkSpawn()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+        inputReader.PrimaryFireEvent += HandlePrimaryFire;
+    }
+
+    public override void OnNetworkDespawn()
+    {
+        if (!IsOwner)
+        {
+            return;
+        }
+        inputReader.PrimaryFireEvent -= HandlePrimaryFire;
+    }
+    
+    private void Update()
+    {
+        if(_flashTimer>0f)
+        {
+            _flashTimer -= Time.deltaTime;
+            if(_flashTimer<=0f)
+            {
+                flashPrefab.SetActive(false);
+            }
+        }
+        
+        if(!IsOwner)
+        {
+            return;
+        }
+
+        if (!_shouldFire)
+        {
+            return;
+        }
+
+        if (Time.time < _prevFireTime+(1f / fireRate))
+        {
+            return;
+        }
+        
+        PrimaryFireServerRpc(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        SpawnDummyProjectile(projectileSpawnPoint.position, projectileSpawnPoint.up);
+        
+        _prevFireTime = Time.time;
+    }
+    
+    private void HandlePrimaryFire(bool shouldFire)
+    {
+        this._shouldFire = shouldFire;
+    }
+
+    [ServerRpc]
+    private void PrimaryFireServerRpc(Vector3 spawnPos, Vector3 direction)
+    {
+        GameObject projectileInstance = Instantiate(serverProjectilePrefab, spawnPos, Quaternion.identity);
+        
+        projectileInstance.transform.up = direction;
+        
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+        
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up * projectileSpeed;
+        }
+        
+        SpawnDummyProjectileClientRpc(spawnPos, direction);
+    }
+
+    [ClientRpc]
+    private void SpawnDummyProjectileClientRpc(Vector3 spawnPos, Vector3 direction)
+    {
+        if (IsOwner)
+        {
+            return;
+        }
+        SpawnDummyProjectile(spawnPos, direction);
+    }
+    
+    private void SpawnDummyProjectile(Vector3 spawnPos, Vector3 direction)
+    {
+        flashPrefab.SetActive(true);
+        _flashTimer = flashDuration;
+        
+        GameObject projectileInstance = Instantiate(clientProjectilePrefab, spawnPos, Quaternion.identity);
+        
+        projectileInstance.transform.up = direction;
+        
+        Physics2D.IgnoreCollision(playerCollider, projectileInstance.GetComponent<Collider2D>());
+
+        if (projectileInstance.TryGetComponent<Rigidbody2D>(out Rigidbody2D rb))
+        {
+            rb.linearVelocity = rb.transform.up * projectileSpeed;
+        }
+    }
+}
---
+++ Assets/Scripts/Utility/Lifetime.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Lifetime : MonoBehaviour
+{
+    [SerializeField] private float lifetime = 5f;
+    private void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+}
